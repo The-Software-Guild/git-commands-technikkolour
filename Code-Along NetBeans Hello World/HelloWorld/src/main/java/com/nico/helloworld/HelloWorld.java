ac0573bf9aaab0fc71d65bf58cd9c8be72610996
Nico Oprescu <oprescunico@gmail.com>
2022-10-11 17:12:10 +0300

    Restructured Repo and Added NetBeans Project

---
+++ Code-Along NetBeans Hello World/HelloWorld/src/main/java/com/nico/helloworld/HelloWorld.java
@@ -0,0 +1,15 @@
+/**
+ *@author Nico
+ *email: oprescunico@gmail.com
+ *date: 11.10.2022
+ *purpose: My first Hello World program in an IDE
+ */
+
+// Use YOUR Group Id value here if it is different from com.mthree
+package com.nico.helloworld; 
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, Nico!");
+    }
+}
